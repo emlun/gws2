e001d33b00f3adc53b4b4e51db1d51421fa61f1d
Emil Lundberg <emil@emlun.se>
2018-05-25 18:11:45 +0200

    Add method Project.remotes()

--- src/config/data/project.rs
+++ src/config/data/project.rs
@@ -23,4 +23,54 @@ impl Project {
         }
     }
 
+    pub fn remotes(&self) -> Vec<&Remote> {
+        let mut extras: Vec<&Remote> = self.extra_remotes.iter().collect();
+        let mut result = vec![&self.main_remote];
+        result.append(&mut extras);
+        result
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Project;
+    use super::Remote;
+
+    #[test]
+    fn remotes_returns_main_remote_and_then_extras() {
+        assert_eq!(
+            Project {
+                path: "foo".to_string(),
+                main_remote: Remote {
+                    name: "origin".to_string(),
+                    url: "git@github.com:foo/boo.git".to_string(),
+                },
+                extra_remotes: vec![
+                    Remote {
+                        name: "myone".to_string(),
+                        url: "http://coool".to_string(),
+                    },
+                    Remote {
+                        name: "upstream".to_string(),
+                        url: "testurl".to_string(),
+                    },
+                ],
+            }.remotes(),
+            vec![
+                Remote {
+                    name: "origin".to_string(),
+                    url: "git@github.com:foo/boo.git".to_string(),
+                },
+                Remote {
+                    name: "myone".to_string(),
+                    url: "http://coool".to_string(),
+                },
+                Remote {
+                    name: "upstream".to_string(),
+                    url: "testurl".to_string(),
+                },
+            ].iter().collect::<Vec<&Remote>>()
+        );
+    }
 }
