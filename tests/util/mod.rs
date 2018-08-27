ab5a1d760223e31bf7a3d04f44a56d6ad620b7ca
Emil Lundberg <emil@emlun.se>
2018-08-27 20:36:27 +0200

    Add Error wrapper type IoErrorOrGit2Error

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -3,7 +3,6 @@ extern crate gws2;
 extern crate tempdir;
 
 use std::fs::copy;
-use std::io::Error;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
@@ -14,11 +13,36 @@ use gws2::config::data::Workspace;
 use gws2::config::read::read_workspace_file;
 
 
-pub fn in_example_workspace<T>(test: fn(&Path, Workspace) -> Result<T, Error>) {
-    assert!(in_example_workspace_inner(test).is_ok());
+#[derive(Debug)]
+pub enum IoErrorOrGit2Error {
+    IoError(::std::io::Error),
+    Git2Error(::git2::Error)
 }
 
-fn in_example_workspace_inner<T>(test: fn(&Path, Workspace) -> Result<T, Error>) -> Result<T, Error> {
+impl From<::std::io::Error> for IoErrorOrGit2Error {
+    fn from(e: ::std::io::Error) -> IoErrorOrGit2Error {
+        IoErrorOrGit2Error::IoError(e)
+    }
+}
+
+impl From<::git2::Error> for IoErrorOrGit2Error {
+    fn from(e: ::git2::Error) -> IoErrorOrGit2Error {
+        IoErrorOrGit2Error::Git2Error(e)
+    }
+}
+
+pub fn in_example_workspace<T>(
+    test: fn(&Path, Workspace) -> Result<T, IoErrorOrGit2Error>
+) {
+    let result = in_example_workspace_inner(test);
+    assert!(result.is_ok(), format!("{:?}", result.err()));
+}
+
+fn in_example_workspace_inner<T, E>(
+    test: fn(&Path, Workspace) -> Result<T, E>
+) -> Result<T, IoErrorOrGit2Error>
+    where IoErrorOrGit2Error: From<E>
+{
     let tmpdir = TempDir::new("gws2-test")?;
 
     let projects_gws_path: PathBuf = Path::new("tests").join("test_projects.gws");
@@ -42,5 +66,5 @@ fn in_example_workspace_inner<T>(test: fn(&Path, Workspace) -> Result<T, Error>)
 
     let workspace = read_workspace_file(tmpdir.path().join(".projects.gws")).unwrap();
     let result = test(tmpdir.path(), workspace);
-    result.map_err(|e| Error::new(::std::io::ErrorKind::Other, e))
+    result.map_err(|e| IoErrorOrGit2Error::from(e))
 }
