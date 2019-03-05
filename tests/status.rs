93974bc6d84fde6b71cc501abf4f66360d61a72c
Emil Lundberg <emil@emlun.se>
2019-03-06 00:16:20 +0100

    Add test of --only-changes

--- tests/status.rs
+++ tests/status.rs
@@ -174,3 +174,108 @@ fn status_produces_correct_data_structure() {
         Ok(())
     });
 }
+
+#[test]
+fn status_ignores_clean_repos_with_only_changes() {
+    in_example_workspace(|working_dir, workspace| {
+        let command = Status { only_changes: true };
+
+        let project_stati: Vec<Result<RepositoryStatus, Error>> = command
+            .make_report(working_dir, &workspace)
+            .into_iter()
+            .map(|(_, s)| s)
+            .collect();
+
+        assert_eq!(
+            project_stati,
+            vec![
+                // changes/changed_files
+                Ok(tree_set(vec![
+                    BranchStatus {
+                        name: "master".to_string(),
+                        upstream_name: Some("origin/master".to_string()),
+                        dirty: DirtyState::UncommittedChanges,
+                        is_head: true,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    },
+                    BranchStatus {
+                        name: "master2".to_string(),
+                        upstream_name: Some("ahead/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: false,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    }
+                ])),
+                // changes/new_files
+                Ok(tree_set(vec![
+                    BranchStatus {
+                        name: "master".to_string(),
+                        upstream_name: Some("origin/master".to_string()),
+                        dirty: DirtyState::UntrackedFiles,
+                        is_head: true,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    },
+                    BranchStatus {
+                        name: "master2".to_string(),
+                        upstream_name: Some("ahead/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: false,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    }
+                ])),
+                // new_commit/local
+                Ok(tree_set(vec![
+                    BranchStatus {
+                        name: "master".to_string(),
+                        upstream_name: Some("origin/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: true,
+                        in_sync: Some(false),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    },
+                    BranchStatus {
+                        name: "master2".to_string(),
+                        upstream_name: Some("ahead/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: false,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    }
+                ])),
+                // new_commit/remote
+                Ok(tree_set(vec![
+                    BranchStatus {
+                        name: "master".to_string(),
+                        upstream_name: Some("origin/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: true,
+                        in_sync: Some(true),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    },
+                    BranchStatus {
+                        name: "master2".to_string(),
+                        upstream_name: Some("ahead/master".to_string()),
+                        dirty: DirtyState::Clean,
+                        is_head: false,
+                        in_sync: Some(false),
+                        upstream_fetched: false,
+                        fast_forwarded: false,
+                    }
+                ])),
+            ]
+        );
+
+        Ok(())
+    });
+}
