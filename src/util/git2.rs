7d7249cd775d593114571cadf9a95ec40bad2f02
Emil Lundberg <emil@emlun.se>
2019-05-28 04:07:14 +0200

    Fix stop conditions in is_descendant_of

--- src/util/git2.rs
+++ src/util/git2.rs
@@ -30,37 +30,53 @@ pub trait WithAncestors<'repo> {
         let mut ancestors_seen: HashSet<Oid> = HashSet::new();
         let mut self_ancestors = self.ancestors();
         let mut other_ancestors = other.ancestors();
+        let mut other_ancestors_seen: HashSet<Oid> = HashSet::new();
 
+        // Walk backwards in history from both self and other, in parallel, one
+        // commit at a time.
         loop {
             match (self_ancestors.next(), other_ancestors.next()) {
                 (Some(sa), Some(oa)) => {
                     if sa.id() == other_id {
+                        // If at any point we encounter `other` as an ancestor
+                        // of `self`, then `other` clearly is an ancestor of
+                        // `self`.
                         return true;
-                    } else if ancestors_seen.contains(&sa.id()) || ancestors_seen.contains(&oa.id())
+                    } else if other_ancestors_seen.contains(&sa.id())
+                        || ancestors_seen.contains(&oa.id())
                     {
+                        // If the current self-ancestor is also an ancestor of
+                        // `other`, or if the current other-ancestor is also an
+                        // ancestor of `self`, then `self` and `other` have a
+                        // common ancestor and `self` cannot be a descendant of
+                        // `other`.
                         return false;
                     } else {
                         ancestors_seen.insert(sa.id());
-                        ancestors_seen.insert(oa.id());
+                        other_ancestors_seen.insert(oa.id());
                     }
                 }
                 (Some(sa), None) => {
                     if sa.id() == other_id {
+                        // If at any point we encounter `other` as an ancestor
+                        // of `self`, then `other` clearly is an ancestor of
+                        // `self`.
                         return true;
-                    } else if ancestors_seen.contains(&sa.id()) {
+                    } else if other_ancestors_seen.contains(&sa.id()) {
+                        // If the current self-ancestor is also an ancestor of
+                        // `other`, then `self` and `other` have a common
+                        // ancestor and `self` cannot be a descendant of
+                        // `other`.
                         return false;
                     } else {
                         ancestors_seen.insert(sa.id());
                     }
                 }
-                (None, Some(oa)) => {
-                    if ancestors_seen.contains(&oa.id()) {
-                        return false;
-                    } else {
-                        ancestors_seen.insert(oa.id());
-                    }
+                (None, _) => {
+                    // If we run out of self-ancestors, then `other` clearly is
+                    // not one of them.
+                    return false;
                 }
-                (None, None) => return false,
             }
         }
     }
