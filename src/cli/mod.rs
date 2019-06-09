738264e00948cca39807efc3afdb24f2527b2b21
Emil Lundberg <emil@emlun.se>
2019-06-09 15:40:32 +0200

    Eliminate single-use variable

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -18,17 +18,17 @@ use crate::crate_info::crate_name;
 use crate::crate_info::crate_version;
 
 pub fn build_cli() -> App<'static, 'static> {
-    let chdir_arg = Arg::with_name("dir")
-        .short("C")
-        .long("chdir")
-        .help("Change to <dir> before doing anything")
-        .takes_value(true);
-
     App::new(crate_name())
         .version(crate_version())
         .about(crate_description())
         .author(crate_author())
-        .arg(chdir_arg)
+        .arg(
+            Arg::with_name("dir")
+                .short("C")
+                .long("chdir")
+                .help("Change to <dir> before doing anything")
+                .takes_value(true),
+        )
         .arg(
             Arg::with_name("no-config")
                 .long("no-config")
