c359d2ca5b4226d4fdba15ac8201f39a920b8491
Emil Lundberg <emil@emlun.se>
2019-08-18 23:51:23 +0200

    Fix sh syntax

--- tests/remove-ssh-key.sh
+++ tests/remove-ssh-key.sh
@@ -8,5 +8,5 @@ GPG_KEYGRIP=000B8A4DF023522536010F9B4E6546DCB7E01A1C
 ssh-add -d tests/id_rsa
 
 if gpg-connect-agent /bye; then
-  gpg-connect-agent <<< "DELETE_KEY --force ${GPG_KEYGRIP}"
+  echo "DELETE_KEY --force ${GPG_KEYGRIP}" | gpg-connect-agent
 fi
